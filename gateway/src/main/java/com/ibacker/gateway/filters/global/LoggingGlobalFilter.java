9a0455cf2a2a21ecae4f4ce99d0c3cc7ed25bf0a
ibacker <ibacker@outlook.com>
2024-12-26 23:31:50 +0800

    请求客户端IP、响应目标地址

--- gateway/src/main/java/com/ibacker/gateway/filters/global/LoggingGlobalFilter.java
+++ gateway/src/main/java/com/ibacker/gateway/filters/global/LoggingGlobalFilter.java
@@ -9,10 +9,12 @@ import org.springframework.http.codec.HttpMessageReader;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.util.List;
 @Slf4j
 @Component
@@ -34,6 +36,19 @@ public class LoggingGlobalFilter implements GlobalFilter, Ordered {
             ServerRequest serverRequest = ServerRequest.create(
                     exchange.mutate().request(cachedRequest).build(), messageReaders
             );
+            // 获取请求 URL 和请求路径
+            URI requestUri = serverRequest.uri();
+            String requestPath = serverRequest.path();
+            log.info("请求 URL: {}", requestUri);
+            log.info("请求路径: {}", requestPath);
+            log.info("客户端 IP: {}", getClientIpFromHeaders(originalRequest));
+
+
+            // 获取请求参数
+            MultiValueMap<String, String> queryParams = serverRequest.queryParams();
+            queryParams.forEach((key, value) -> {
+                log.info("参数名: {}, 参数值: {}", key, value);
+            });
 
             return serverRequest.bodyToMono(String.class)
                     .defaultIfEmpty("")
@@ -52,7 +67,15 @@ public class LoggingGlobalFilter implements GlobalFilter, Ordered {
                         ServerHttpRequest newRequest = (ServerHttpRequest) exchange.getAttributes()
                                 .get(ServerWebExchangeUtils.CACHED_SERVER_HTTP_REQUEST_DECORATOR_ATTR);
 
-                        return chain.filter(exchange.mutate().request(newRequest).build());
+                        return chain.filter(exchange.mutate().request(newRequest).build()).then(Mono.fromRunnable(()->{
+                            // 获取目标地址
+                            URI targetUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+                            if (targetUri != null) {
+                                log.info("路由匹配完成后，目标地址为: {}", targetUri.toString());
+                            } else {
+                                log.warn("路由未匹配成功，无法获取目标地址！");
+                            }
+                        }));
                     });
         });
     }
@@ -60,6 +83,18 @@ public class LoggingGlobalFilter implements GlobalFilter, Ordered {
     @Override
     public int getOrder() {
         return -1; // 优先级，数字越小优先级越高
+    }
+
+    private String getClientIpFromHeaders(ServerHttpRequest request) {
+        String clientIp = request.getHeaders().getFirst("X-Forwarded-For");
+        if (clientIp == null || clientIp.isEmpty() || "unknown".equalsIgnoreCase(clientIp)) {
+            clientIp = request.getHeaders().getFirst("X-Real-IP");
+        }
+        if (clientIp == null || clientIp.isEmpty() || "unknown".equalsIgnoreCase(clientIp)) {
+            // 如果没有代理头部，使用 RemoteAddress
+            clientIp = request.getRemoteAddress() != null ? request.getRemoteAddress().getAddress().getHostAddress() : null;
+        }
+        return clientIp;
     }
 }
 
